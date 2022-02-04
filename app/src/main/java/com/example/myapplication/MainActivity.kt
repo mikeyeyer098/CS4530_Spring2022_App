3c25a67e7069993b1a5431009d5847c60571aeb2
Michael Eyer <mikeyeyer098@gmail.com>
2022-02-04 15:06:09 -0700

    started onClick for create profile

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.load_page)
+
+        val createProfileButton = findViewById<ImageButton>(R.id.createProfile)
+        val nameText = findViewById<
+        createProfileButton.setOnClickListener {
+            Log.d("test", "create profile button pressed")
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Profile.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+class Profile (
+    val name: String,
+    val height: String,
+    val weight: String,
+    val age: Int,
+    val gender: String,
+    val city: String,
+    val picturePath: String,
+){
+
+}
