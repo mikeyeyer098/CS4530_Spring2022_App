78fb36a63fa1f430f4774dc77fc407910835ac35
Michael Eyer <mikeyeyer098@gmail.com>
2022-02-07 16:54:34 -0700

    implemented file saving

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
+import android.widget.EditText
 import android.widget.ImageButton
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.load_page)
 
-        val createProfileButton = findViewById<ImageButton>(R.id.createProfile)
+        val createProfileButton = findViewById<ImageButton>(R.id.createProfileButton)
         createProfileButton.setOnClickListener {
-            Log.d("test", "create profile button pressed")
+            val nameText: String = findViewById<EditText>(R.id.nameTextField).text.toString()
+            val heightText: String = findViewById<EditText>(R.id.heightTextField).text.toString()
+            val weightText: String = findViewById<EditText>(R.id.weightTextField).text.toString()
+            val ageText: String = findViewById<EditText>(R.id.ageTextField).text.toString()
+            val genderText: String = findViewById<EditText>(R.id.genderTextField).text.toString()
+            val cityText: String = findViewById<EditText>(R.id.cityTextField).text.toString()
+
+            val profile = Profile(nameText, heightText, weightText, ageText, genderText, cityText, "")
+            Log.i ("test", profile.printForStoring())
+            Log.i("test", applicationContext.cacheDir.absolutePath)
+            File.createTempFile("filename", profile.printForStoring(), applicationContext.cacheDir)
         }
     }
 }
