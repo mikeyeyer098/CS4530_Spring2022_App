6d2b6258b3dfafaaedd5aeec2e7af3e0c8cac6e4
Rory Donald <37783072+Donalror@users.noreply.github.com>
2022-02-07 17:18:55 -0700

    Asks for Camera permissions and updates image view when taking picture

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,65 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONCODE = 100
+    private val CAMERACODE = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.load_page)
 
+        val takePictureButton = findViewById<ImageButton>(R.id.TakePictureButton)
         val createProfileButton = findViewById<ImageButton>(R.id.createProfile)
         createProfileButton.setOnClickListener {
             Log.d("test", "create profile button pressed")
+        }
+
+        takePictureButton.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERACODE)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), PERMISSIONCODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode ==  PERMISSIONCODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERACODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val profilePicImageView = findViewById<ImageView>(R.id.ProfilePicImageView)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == CAMERACODE) {
+            val picDisplay: Bitmap = data!!.extras!!.get("data") as Bitmap
+            profilePicImageView.setImageBitmap(picDisplay)
         }
     }
 }
