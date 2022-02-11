6b98755947fe4b4637bd00ef33aafff1b0a0484a
audreymiller12 <u1197148@utah.edu>
2022-02-11 11:48:52 -0700

    Changed home screen, also changed BMI calc

--- app/src/main/java/com/example/myapplication/BMI_calculator.kt
+++ app/src/main/java/com/example/myapplication/BMI_calculator.kt
@@ -1,15 +1,14 @@
 package com.example.myapplication
 
-import android.graphics.Bitmap
-import android.media.Image
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
-import android.content.Intent
 import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,10 +18,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [home_page.newInstance] factory method to
+ * Use the [BMI_calculator.newInstance] factory method to
  * create an instance of this fragment.
  */
-class home_page : Fragment() {
+class BMI_calculator : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,28 +33,34 @@ class home_page : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view: View = inflater.inflate(R.layout.fragment_home_page, container, false)
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b_m_i_calculator, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        var profileThumb = requireView().findViewById<ImageButton>(R.id.ProfilePicThumbnail)
 
-        val mapsButton = view.findViewById(R.id.HikesFinderButton) as ImageButton
-        mapsButton.setOnClickListener {
-            val gmmIntentUri = Uri.parse("geo:0,0?q=hikes")
-            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
-            mapIntent.setPackage("com.google.android.apps.maps")
-            startActivity(mapIntent)
-        }
+        var profileThumb = requireView().findViewById<ImageButton>(R.id.ProfilePicThumbnail)
         profileThumb.setImageBitmap(profile?.image)
+
+        var heightTag = requireView().findViewById<TextView>(R.id.heightTextField)
+        heightTag.hint = "Height: ${profile?.height}"
+
+        var weightTag = requireView().findViewById<TextView>(R.id.weightTextField)
+        weightTag.hint = "Weight: ${profile?.weight}"
+
+        var BMItag = requireView().findViewById<TextView>(R.id.BMIText)
+        BMItag.text = HealthCalculator().calculateBMI(profile?.weight.toString(), profile?.height.toString())
+
+        val BMiButton = view.findViewById(R.id.CalculateBMIButton) as Button
+        BMiButton.setOnClickListener {
+            BMItag.text = HealthCalculator().calculateBMI(profile?.weight.toString(), profile?.height.toString())
+        }
 
         super.onViewCreated(view, savedInstanceState)
     }
@@ -67,15 +72,13 @@ class home_page : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment home_page.
+         * @return A new instance of fragment BMI_calculator.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(profile: Profile) =
-            home_page().apply {
-                this.profile = profile
+        fun newInstance(param1: String, param2: String) =
+            BMI_calculator().apply {
                 arguments = Bundle().apply {
-
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
