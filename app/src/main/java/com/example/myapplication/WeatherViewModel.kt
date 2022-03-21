fc44bad73b5ea560ad04f2d5cea8b0a6ff51289d
audreymiller12 <u1197148@utah.edu>
2022-03-21 15:50:57 -0600

    added implementations for SQLite, Room and ViewModel and added connection to weather repo and vm

--- app/src/main/java/com/example/myapplication/WeatherViewModel.kt
+++ app/src/main/java/com/example/myapplication/WeatherViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.myapplication
 
-class WeatherViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class WeatherViewModel(
+    private val weatherRepo: WeatherRepo
+): ViewModel() {
+    fun getWeatherData(loc : String){
+        viewModelScope.launch {
+
+            val result = try {
+                weatherRepo.makeWeatherRequest(loc)
+            } catch(e: Exception) {
+                Result.Error(Exception("Network request failed"))
+            }
+        }
+    }
 }
